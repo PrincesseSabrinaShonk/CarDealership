b29bf42d4d42bef23a4066c77a1ff3c26a2517fe
Sabrina D. <sabrinadorzin103@gmail.com>
2025-10-23 16:15:54 -0400

    Phase one completed updated

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,12 +2,7 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        Dealership dealer = new Dealership("Kia Dealership", "11410 ", "509-515-1603");
-
-        Vehicle car = new Vehicle("AVG423", "jeep", "Big", 2025, 2500);
-        dealer.addVehicle(car);
-
-        System.out.println("Vehicles in inventory: " + dealer.getAllVehicles().size());
+        System.out.println("hello");
 
         }
     }
